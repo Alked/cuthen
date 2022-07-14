8922329bb05df2794565b23760242132c0026841
Alked <ytan3018@uni.sydney.edu.au>
2022-07-14 18:44:23 +0800

    feat [Alked Tang] #2 Prepared basic Vue 3 project scaffold

---
+++ hydrogen/src/views/ScheduleView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <sched />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Sched from '@/components/sched/Sched.vue';
+
+export default {
+  name: 'ScheduleView',
+  components: {
+    Sched,
+  },
+};
+</script>
