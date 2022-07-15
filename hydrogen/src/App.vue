59e7369c464f1f7ed2df21452943d96c0901e6c7
Alked <ytan3018@uni.sydney.edu.au>
2022-07-15 15:20:58 +0800

    feat [Alked Tang] #4 Top bar built with parameterised title and version number. - Included Montserrat font family

--- hydrogen/src/App.vue
+++ hydrogen/src/App.vue
@@ -1,9 +1,33 @@
 <template>
-  <!-- TODO: page layout -->
+  <top-bar title="CUThen" version="v0.0.1"/>
   <router-view/>
 </template>
 
+<script>
+import TopBar from './components/common/topbar/TopBar.vue';
+
+export default {
+  components: {
+    TopBar,
+  },
+};
+</script>
+
 <style>
+@font-face {
+  font-family: "Montserrat";
+  src: local("Montserrat"),
+    url("./assets/fonts/Montserrat/Montserrat-Regular.ttf")
+      format("truetype");
+}
+
+body {
+  margin: 0;
+  background: #2d2d2d;
+  display: flex;
+  justify-content: center;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
